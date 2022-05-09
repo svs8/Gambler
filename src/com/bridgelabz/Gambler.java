970f73019d2de443947bbdc0f40f6ef8b9c97703
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 20:05:36 +0530

    Added:first commit

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Gambler {
+    static final int STAKEPERDAY=100;
+    static final int BETPERGAME=1;
+
+        public static void main(String[] args) {
+            System.out.println("Stake per day is "+ STAKEPERDAY );
+            System.out.println("Bet per Game is "+ BETPERGAME );
+
+        }
+    }
+    
