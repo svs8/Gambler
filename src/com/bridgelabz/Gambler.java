f169def8935fc6b81a8614e45a239a86ec299186
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 21:07:02 +0530

    Added:If Won Or Lost will resign for the day if its 50 percent of stake

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -3,27 +3,34 @@ package com.bridgelabz;
 public class Gambler {
     static final int STAKEPERDAY=100;
     static final int BETPERGAME=1;
+    static int gamblerearn=100;
 
         public static void main(String[] args) {
-            System.out.println("Stake per day is "+ STAKEPERDAY );
-            System.out.println("Bet per Game is "+ BETPERGAME );
-               int gamblerearn=0;
-               int gamblerlost=0;
-                int game = (int) (Math.random() * 10) % 2;
-                if(game==1)
-                {
+            System.out.println("Stake per day is " + STAKEPERDAY);
+            System.out.println("Bet per Game is " + BETPERGAME);
 
-                    gamblerearn=STAKEPERDAY+1;
-                    System.out.println("Gambler won the game and the amount now is "+gamblerearn);
+            while (gamblerearn > 50 && gamblerearn < 150) {
+                int bet = (int) (Math.random() * 10) % 2 +1;
+                switch (bet) {
+                    case 1:
+                        gamblerearn++;
+                        System.out.println("Gambler won the game and the amount now is " + gamblerearn);
+                        if (gamblerearn == 150) {
+                        System.out.println("Gambler resigned for the day with " + gamblerearn);
+                       }
+                         break;
 
-                }
-                else{
-                    gamblerlost=STAKEPERDAY-1;
-                    System.out.println("gambler lost the game and the amount now is "+gamblerlost);
+                    case 2:
+                           gamblerearn--;
+                           System.out.println("Gambler lost the game and the amount now is " + gamblerearn);
+                           if (gamblerearn == 50) {
+                             System.out.println("Gambler resigned for the day with " + gamblerearn);
+                    }
                 }
 
 
             }
 
         }
+    }
 
